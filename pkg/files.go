b5c9bd2af5f81d015ae0ba48a1e9e52f31fa43f2
Nikita Ronja <nikita@NetBSD.org>
2025-02-09 20:46:29 +0100

    LineCounter

--- pkg/files.go
+++ pkg/files.go
@@ -1,10 +1,16 @@
 package pkg
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
+	"fmt"
+	"github.com/phuslu/log"
+	"io"
 	"log/slog"
 	"os"
 	"os/exec"
+	"time"
 )
 
 // CheckFile checks if the given file `s` exists.
@@ -19,13 +25,64 @@ func CheckFile(s string) bool {
 func CheckExecutable(s string, logger *slog.Logger) (bool, error) {
 	path, err := exec.LookPath(s)
 	if err != nil {
-		logger.Debug("", slog.Any("error", err))
+		switch logger {
+		case nil:
+			_, _ = fmt.Fprint(os.Stderr, "%w\n", err)
+		default:
+			logger.Debug("", slog.Any("error", err))
+		}
 	}
-	logger.Debug("file found", slog.String("file", s), slog.String("path", path))
+	switch logger {
+	case nil:
+		_, _ = fmt.Fprintf(os.Stderr, "file found: %q at path %q\n", s, path)
+	default:
+		logger.Debug("file found", slog.String("file", s), slog.String("path", path))
+	}
 	cmd := exec.Command(s)
 	if err := cmd.Run(); err != nil {
 		return false, err
 	}
-	logger.Debug("file is executable", slog.String("file", s))
+	switch logger {
+	case nil:
+		_, _ = fmt.Fprintf(os.Stderr, "file is executable: %q\n", s)
+	default:
+		logger.Debug("file is executable", slog.String("file", s))
+	}
 	return true, nil
+}
+
+// LineCounter counts the number of lines in a file in a sufficiently fast way.
+func LineCounter(logger *log.Logger, r io.Reader, tFlag ...bool) (int, error) {
+	start := time.Now()
+	if len(tFlag) > 0 {
+		switch logger {
+		case nil:
+			defer TrackExecutionTime(nil, start)
+		default:
+			defer TrackExecutionTime(logger, start)
+		}
+	}
+	var c int
+	buf := make([]byte, bufio.MaxScanTokenSize)
+
+	for {
+		bufferSize, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return 0, err
+		}
+
+		var buffPosition int
+		for {
+			i := bytes.IndexByte(buf[buffPosition:], '\n')
+			if i == -1 || bufferSize == buffPosition {
+				break
+			}
+			buffPosition += i + 1
+			c++
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+	return c, nil
 }
---
+++ pkg/files_test.go
@@ -0,0 +1,29 @@
+package pkg_test
+
+import (
+	"bufio"
+	"github.com/nikkicoon/utility-go/pkg"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestCheckFile(t *testing.T) {
+	assert.True(t, pkg.CheckFile("../counter_test"))
+	assert.False(t, pkg.CheckFile("../END"))
+}
+
+func TestCheckExecutable(t *testing.T) {
+	ok, err := pkg.CheckExecutable("/bin/sh", nil)
+	assert.True(t, ok)
+	assert.Nil(t, err)
+}
+
+func TestLineCounter(t *testing.T) {
+	file, err := os.Open("../counter_test")
+	assert.Nil(t, err)
+	reader := bufio.NewReader(file)
+	count, err := pkg.LineCounter(nil, reader, true)
+	assert.Nil(t, err)
+	assert.Equal(t, count, 5)
+}
